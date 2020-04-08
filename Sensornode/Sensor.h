1852126790f07905f989407638a33cdb0860144c
schimen <einesimen@gmail.com>
2020-04-09 00:52:59 +0200

    mye nye kommentarer, ny funksjonalitet for maksimum lovlig verdi

--- Sensornode/Sensor.h
+++ Sensornode/Sensor.h
@@ -1,30 +1,51 @@
+/* * * * * * * * * * *     Sensor class     * * * * * * * * * * */
 
 #ifndef Sensor_h
 #define Sensor_h
-#define MAX_READ 4096
-#define MIN_READ 0
 
 #include <Arduino.h>
 
 class Sensor {
   public:
-    Sensor(uint8_t sensorPin);
-    void     read();
+
+    //constructor of Sensor class
+    //                   pin, max allowed for sensor
+    Sensor(uint8_t sensorPin, uint16_t max);
+
+    //read sensor.
+    //on read, check if over max, check for new average and store read value
+    void read();
+
+    //return sensor value
     uint16_t getValue();
-    uint16_t getAverage(bool resetCount=true);
-    bool     newAverage;
-    bool     newMaxMin;
-    static uint16_t getMax();
-    static uint16_t getMin();
-    static uint8_t  averageCount;
-    static uint16_t maxValue;
-    static uint16_t minValue;
+
+    //return average value (cumulativeValue / readCount)
+    //bool reset will reset used variables if true
+    //          (readCount, cumulativeValue and newAverage)
+    uint16_t getAverage(bool reset = true);
+
+    //return max value (maxValue)
+    //bool reset will set maxValue to minimum if true
+    static uint16_t getMax(bool reset = true);
+
+    //return min value (minValue)
+    //bool reset will set minValue to maximum if true
+    static uint16_t getMin(bool reset = true);
+
+    //public variables
+    bool            newAverage;  //signal if new average value is available
+    static uint8_t  averageCount;   //number of reads for new average value
+    static uint8_t  sensorsOverMax; //number of sensors over maxAllowed value
+    static uint16_t maxValue; //maximum value of all sensors
+    static uint16_t minValue; //minimum value of all sensors
 
   private:
+    bool     overMax; //signal if sensor is over max allowed value
     uint8_t  pin;
-    uint8_t  readCount;
-    uint16_t sensorValue;
-    uint32_t cumulativeValue;
+    uint8_t  readCount; //number of reads of sensor (resets on getAverage())
+    uint16_t sensorValue; //current sensor value
+    uint16_t maxAllowed;  //maximum value allowed for sensor
+    uint32_t cumulativeValue; //cumulative sensor value (reset on getAverage())
 };
 
 #endif
