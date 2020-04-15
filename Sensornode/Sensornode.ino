02bbca32c54bfbce2c4ed299b347a96ad25d26be
Xinini <Laro7500@gmail.com>
2020-04-15 16:43:44 +0200

    Testing loading webpage

--- Sensornode/Sensornode.ino
+++ Sensornode/Sensornode.ino
@@ -273,27 +273,49 @@
 //   timer.run();  //starts all enabled timers on timer object
 // }
 
-#include "SPIFFS.h"
+
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+
+
+const char* ssid = "Marco";
+const char* pass = "7Mgb67HK";
+
+AsyncWebServer server (80); //Create WebServer object on port 80
+
  
 void setup() {
   Serial.begin(115200);
   
-  if(!SPIFFS.begin(true)){
+  if(!SPIFFS.begin(true)){ //Upload the data files
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
+
   }
   
-  File file = SPIFFS.open("/test_example.txt");
-  if(!file){
-    Serial.println("Failed to open file for reading");
-    return;
+  WiFi.begin(ssid, pass);
+  Serial.println("Connecting...");
+  while (WiFi.status() != WL_CONNECTED){ //Wait for connection
+    delay(1000);
+    Serial.println(".");
   }
-  
-  Serial.println("File Content:");
-  while(file.available()){
-    Serial.write(file.read());
-  }
-  file.close();
+  Serial.println("CONNECTED!");
+  Serial.println(WiFi.localIP());
+
+  //Route for webpage
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html");
+  });
+  //Route for images
+  server.on("/blynk_bootlegLONG.svg", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/blynk_bootlegLONG.svg");
+  });
+  server.on("/midjoo.png", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/midjoo.png");
+  });
+
+  server.begin();
 }
  
 void loop() {
