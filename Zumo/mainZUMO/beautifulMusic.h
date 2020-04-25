b8f6584690792c7139504a5de87cfaa1402a46c4
Epsilonium <64090039+Epsilonium@users.noreply.github.com>
2020-04-25 20:17:42 +0200

    Update beautifulMusic.h
    La til litt ekstra stuff

--- Zumo/mainZUMO/beautifulMusic.h
+++ Zumo/mainZUMO/beautifulMusic.h
@@ -26,3 +26,61 @@ const char GoT [] PROGMEM =
   " L8 d<g<b-16c16 d<g<b-16c16 d<g<b-16c16d4."
   " L4 f.<b-.e-16d16fb-e-16d16"
   " O3 L8 >cfa-16b-16 >cfa-16b-16 >cfa-16b-16>c4.";
+
+const char Morgenstemning[] PROGMEM =
+  "!T60 L8 gedcdegedcd16e16d16e16gegaeagedc2";
+
+const char StarWars [] PROGMEM =
+  "!T112 L16 d8.dg2>d2>cba"
+  "O5 g2rd4c<b<ag2rd4c<bc<a1";
+
+const char Prelude [] PROGMEM =
+  "!T130 L16 O5 cgfge-gfgcgfge-gfg"
+  " ca-ga-fa-ga-ca-ga-fa-ga-ca-ga-fa-ga-ca-ga-fa-ga-" 
+  " ce-de-ge-de-ce-de-ge-de-"
+  " c>cb->ca->cb->c c>cb->ca->cb->c"
+  " cagag-aga cagag-aga"
+  " <b-b-ab-gb-ab- <b-b-ab-gb-ab-"
+  " <b-gfgegfg <b-gfgegfg"
+  " <a-a-ga-fa-ga- <a-a-ga-fa-ga-"
+  " <a-dcdfdcd <a-dcdfdcd"
+  " <ge-de-ge-de- <ge-de-ge-de-"
+  " ce-de-a-e-de- ce-de-a-e-de-"
+  " dfe-fa-fe-f dfe-fa-fe-f"
+  " e-gfga-gfg e-gfga-gfg"
+  " e-agafaga e-agafaga"
+  " dfe-fa-fe-f dfe-fa-fe-f"
+  " cfefa-fef cfefa-fef"
+  " ce-de-fe-de- ce-de-fe-de-"
+  " <a-e-de-ce-de- <a-e-de-ce-de-"
+  " <ge-de-fe-de- <ge-de-fe-de-"
+  " <ge-de-ce-de- <ge-de-ce-de-"
+  " <ge-de-fe-de- <ge-de-fe-de- <ge-de-fe-de- <ge-de-fe-de-"
+  " O4 <g<bdfa-fefbf>dba-fef"
+  " <gce-g>cgf+g O5 e-cge-c<g<f+"
+  " O4 <gaf+ O5 ce-c<bcgcag-e-c<bc"
+  " O3 g2a-2f2g2";
+
+const char CrazyTrain [] PROGMEM =
+  "!T124 O3 L8 f#f#>c#>df#>c#>c#"
+  " bag#abag#e"
+  " f#f#>c#>df#>c#>c#"
+  " bag#abag#e"
+  " f#f#>c#>df#>c#>c#"
+  " bag#abag#e"
+  " f#f#>c#>df#>c#>c#>d2>e2";
+
+const char Dragnet[] PROGMEM =
+  "!T80 L8 c.d16e-16r16cR2c.d16e-16r16cg-2";
+
+const char Rosanna [] PROGMEM = 
+  "!T166 O3 L8 >e->e-4 b-b-4 ff4"
+  " O4 L16 <g#<acdf8g#gfd8fr8"
+  " T83 O5 L8 cdde-16e-16r8de-16e-16r8.r8"
+  " O3 L16 >e->e-8.b-b-8.ff8.r8"
+  " O5 L8 cdde-16e-16r8de-16e-16r8.r8"
+  " L16 e-e-8.dd8.cc8.r8"
+  " L8 cdde-16e-16r8de-16e-16r8.r8"
+  " O3 L16 >e->e-8.b-b-8.ff8.r8"
+  " O5 L8 cdde-16e-16r8de-16e-16r8.r8"
+  " O4 L16 e-e-8.<b-<b-8.ff8.r8";
